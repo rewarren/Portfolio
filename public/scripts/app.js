5e5a45d2188d91ba5bcfb950fa968e5042f8f849
Rachel Warren <raelwa@gmail.com>
2016-03-09 10:57:43 -0500

    Added service to grab project.json data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,27 @@
 angular.module('portfolioSite',[])
 
-.controller('mainCtrl', function($scope){
+.controller('mainCtrl', function($scope, projectService){
   $scope.helloWorld = function(){
     console.log("hello world!! About me")
   }
 
-  $scope.projects = function(){
-    console.log("take me to projects!")
+  // $scope.projects = function(){
+  //   console.log("take me to projects!")
+  // }
+
+  projectService.getProjects(function(response){
+    console.log(response.data)
+    var projects = response.data
+    $scope.projects = projects;
+  })
+
+})
+
+.service('projectService', function($http){
+
+  this.getProjects = function(callback){
+    $http.get('mock/projects.json')
+    .then(callback)
   }
 
-  $scope.projects = [
-    {
-      "name":"Elephant Trivia",
-      "applink":"http://rewarren.github.io/Elephant-Trivia/",
-      "github":"https://github.com/rewarren/Elephant-Trivia",
-      "blurb":"Built with HTML, CSS and jQuery, a fun and informative trivia quiz about, you guessed it, Elephants!",
-      "photoUrl":"images/projects/eletrivia.jpg"
-    },
-    {
-      "name":"Where Would Silvia Go",
-      "applink":"https://wwsg.herokuapp.com/",
-      "github":"https://github.com/rewarren/wwsg",
-      "blurb":"a Ruby on Rails application inspired by my friend Silvia, who always had a restaurant recommendation ready for anyone who asked.",
-      "photoUrl":"images/projects/wwsg.jpg"
-    },
-    {
-      "name":"WiFinder",
-      "applink":"https://wifinder-app.herokuapp.com/",
-      "github":"https://github.com/rewarren/wifinder",
-      "blurb":"Ever want to work on your laptop out of the house, but not sure where to go? WiFinder allows you to find, rate and comment on local spots with available wifi for those on-the-go needs. It was built in collaboration between WDI and UX students at General Assembly.",
-      "photoUrl":"images/projects/wifinder.jpg"
-    }
-  ]
-
-
-});
+})
